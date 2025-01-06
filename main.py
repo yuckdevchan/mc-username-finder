ce0b707b5965f8ae0cf808f8855d4c0c5d52e7c3
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2025-01-06 16:36:49 +0000

    changes

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def is_username_taken(username):
     while not found:
         response = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{username}")
         if response.status_code == 429:
-            time.sleep(2) # Rate wait
+            print("mcuf: Rate limit exceeded.")
+            # time.sleep(2) # Rate wait
         else:
             break
     return response.status_code == 200, response.status_code
@@ -17,24 +18,29 @@ def is_username_taken(username):
 def find_usernames_of_length(length):
     word_list = words.words()
     word_list = [word.lower() for word in word_list]
-    with open("usernames.txt", "r") as file:
-        last_name = file.readlines()[-1].strip()
-    word_list = word_list[word_list.index(last_name) + 1:]
+    try:
+        with open(f"usernames{str(length)}.txt", "r") as file:
+            last_name = file.readlines()[-1].strip()
+        word_list = word_list[word_list.index(last_name) + 1:]
+    except FileNotFoundError:
+        pass
 
     for word in word_list:
         if len(word) == length:
             taken, status = is_username_taken(word)
-            if not taken:
-                print(f"Username '{word}' is available ({status})")
-                if os.path.exists("usernames.txt"):
-                    with open("usernames.txt", "a") as file:
+            if taken:
+                print(f"mcuf: Username '{word}' is taken ({status})")
+            else:
+                print(f"mcuf: Username '{word}' is available ({status})")
+                if os.path.exists(f"usernames{str(length)}.txt"):
+                    with open(f"usernames{str(length)}.txt", "a") as file:
                         file.write(word + "\n")
                 else:
-                    with open("usernames.txt", "w") as file:
+                    with open(f"usernames{str(length)}.txt", "w") as file:
                         file.write(word + "\n")
-            time.sleep(0.4) # Avoid rate limit
+            # time.sleep(0.4) # Avoid rate limit
 
 if __name__ == "__main__":
     nltk.download('words')
-    length = int(input("Enter the length of the username: "))
+    length = int(input("mcuf: Enter the length of the username: "))
     find_usernames_of_length(length)
